9f67d76b36b316dbac0aff7ced5af8c1a4bab9d4
Wei <t108598023@ntut.edu.tw>
2022-03-09 16:43:12 +0800

    remake

--- README.md
+++ README.md
@@ -8,22 +8,8 @@ This project uses twitter API to catch Strangestone tweet (Tawawa on monday) and
 Uses cron to schedule for building on Github Action.
 
 ## Setup
-1. Create a telegram bot. [How to Create telegram bot.](https://core.telegram.org/bots#6-botfather)
-2. Get bot id and user id(chat id). [StackOverflow - Telegram Bot - how to get a group chat id?](https://stackoverflow.com/questions/32423837/telegram-bot-how-to-get-a-group-chat-id)
-3. Create a twitter developer project and get bearer_token. [Twitter developer Documentation](https://developer.twitter.com/en/docs/platform-overview)
-4. Clone this project.
-5. Set three environment secrets. Please check secrets name and .github/workflows/main.yml parameters, make sure they are same. [Using environments for deployment](https://docs.github.com/en/actions/deployment/targeting-different-environments/using-environments-for-deployment)
-* **TELEGRAM_TO** : Your telegram userId or chatId.
-* **TELEGRAM_TOKEN** : Your telegram bot token.
-* **TWITTER_BEARER_TOKEN** : Your twitter bearer_token.
-6. Set Github Action and edit .github/workflows/main.yml to test. (cron parameter or option of "on")
-7. (optional) If you want to catch other twitter users' tweet, just edit query in ```api.search_tweets(query="Strangestone")``` and set condition.
 
 
 ## Package
 These packages I use, **THANKS!!**
 
-* [sns-sdks/python-twitter](https://github.com/sns-sdks/python-twitter)
-* [theskumar/python-dotenv](https://github.com/theskumar/python-dotenv)
-* [juliangruber/read-file-action](https://github.com/juliangruber/read-file-action)
-* [appleboy/telegram-action](https://github.com/appleboy/telegram-action)
--- tawawa.py
+++ tawawa.py
@@ -1,13 +1,131 @@
-from pytwitter import Api
+import json
+import requests
 import json
 import re
 import os
 import time
 from dotenv import load_dotenv
 
+# Load env
 load_dotenv()
-api = Api(bearer_token=os.getenv("BEARER_TOKEN"))
+bearer_token = os.getenv("BEARER_TOKEN")
+chatid = os.getenv("CHATID")
 
+def bearer_oauth(r):
+
+	r.headers["Authorization"] = f"Bearer {bearer_token}"
+	r.headers["User-Agent"] = "v2FilteredStreamPython"
+	return r
+
+def get_rules():
+    response = requests.get(
+        "https://api.twitter.com/2/tweets/search/stream/rules", auth=bearer_oauth
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
+        )
+    #print(json.dumps(response.json()))
+    return response.json()
+
+def delete_all_rules(rules):
+    if rules is None or "data" not in rules:
+        return None
+
+    ids = list(map(lambda rule: rule["id"], rules["data"]))
+    payload = {"delete": {"ids": ids}}
+    response = requests.post(
+        "https://api.twitter.com/2/tweets/search/stream/rules",
+        auth=bearer_oauth,
+        json=payload
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Cannot delete rules (HTTP {}): {}".format(
+                response.status_code, response.text
+            )
+        )
+    #print(json.dumps(response.json()))
+
+def set_rules(delete):
+	sample_rules = [
+    	{"value": "月曜日のたわわ　その from:Strangestone OR from:WeiWeiDev"},
+	]
+	payload = {"add": sample_rules}
+	response = requests.post(
+		"https://api.twitter.com/2/tweets/search/stream/rules",
+		auth = bearer_oauth,
+		json = payload,
+	)
+	if response.status_code != 201:
+		raise Exception(
+			"Cannot add rules : {} {}".format(response.status_code, response.text)
+		)
+	#print(json.dumps(response.json()))
+
+def get_stream(set):
+	tweet_fields = "tweet.fields=public_metrics,entities"
+	url = "https://api.twitter.com/2/tweets/search/stream?{}&expansions=author_id".format(tweet_fields)
+	response = requests.get(
+		url,auth=bearer_oauth, stream=True,
+	)
+	#print(response.status_code)
+	if response.status_code != 200:
+		raise Exception(
+			"Cannot add stream, : {} {}".format(response.status_code, response.text)
+		)
+	for response_line in response.iter_lines():
+		if response_line:
+			json_response = json.loads(response_line)
+			# print(json.dumps(json_response, indent=4, sort_keys=True))
+			if "RT " not in json_response["data"]["text"] and "月曜日のたわわ　その" in json_response["data"]["text"]:
+				print(json_response["data"]["text"])
+				apiurl = 'https://api.telegram.org/bot5205675326:AAHRYduNrmmybdlWYftk64ostssY833RVtQ/'
+				telemethod = "sendMessage"
+				chat_id = chatid
+				content = "注意"
+				urlm = '%s%s?chat_id=%s&text=%s' % (apiurl,telemethod,chat_id,content) #messangeurl
+				r = requests.get(urlm)
+				content = json_response["data"]["text"]
+				urlm = '%s%s?chat_id=%s&text=%s' % (apiurl,telemethod,chat_id,content) #messangeurl
+				r = requests.get(urlm)
+				os._exit(0)
+
+def main():
+	rules = get_rules()
+	delete = delete_all_rules(rules)
+	set = set_rules(delete)
+	get_stream(set)
+
+if __name__ == "__main__":
+	main()
+
+#printer.filter(follow=['993160436001292288', '518677431', '93332575', '1501443423026708481'])
+
+
+'''
+# streaming_client = tweepy.StreamingClient(bearer_token)
+class TweetPrinter(tweepy.StreamingClient):
+
+	def on_tweet(self, tweet):
+		if "RT " not in tweet.text:
+			print(tweet.text)
+			os._exit(0)
+		#if "RT " not in tweet.text and "月曜日のたわわ" in tweet.text and "その" in tweet.text: 
+		#	print(tweet.text)
+		#	os._exit(0)
+
+printer = TweetPrinter(bearer_token)
+#printer.add_rules(tweepy.StreamRule("@strangestone"))
+#printer.add_rules(tweepy.StreamRule("@wiwi571246"))
+#printer.filter()
+#printer.add_rules(tweepy.StreamRule("@WeiWeiDev"))
+printer.filter()
+'''
+
+
+
+'''
 oldTweet = os.getenv("OLDTWEET")
 print(oldTweet)
 
@@ -21,7 +139,4 @@ while(True):
 			fp.close()
 			os._exit(0)
 		time.sleep(60)
-
-
-
-
+'''
